cd1a40fe3768f2a79cbe95bb9a83d4810c4f36a5
WuJiUJ <vongvorakul.p@gmail.com>
2022-12-24 17:15:01 +0000

    Auto Server Commit

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def main():
 # PATH=/home/wuji/miniconda3/envs/py310/bin
 # PYTHONPATH=/home/wuji/bot/bybit
 # 58 7,15,23 * * * /home/wuji/bot/bybit/cron.sh >> /home/wuji/bot/bybit/logs/clogs.log 2>&1
+# * * * * * /bin/bash -l -c 'cd /home/wuji/bot/bybit && ./git_push.sh >> /home/wuji/bot/bybit/logs/git_log.log 2>&1'
 
 if __name__ == "__main__":
     main()
