628bc1b5f01fab5824c9bda13529e3dfa0a9ec8a
WuJiUJ <vongvorakul.p@gmail.com>
2022-12-24 18:52:01 +0000

    Auto commit

--- git_push.sh
+++ git_push.sh
@@ -1,6 +1,12 @@
 #!/bin/sh
+
+USERNAME="WuJiUJ"
+PASSWORD="Wuji.!2000"
+REMOTE_REPO="github.com/WuJiUJ/bybit.git"
+EMAIL="email@domain.com"
+
 cd /home/wuji/bot/bybit
 
 git add *
 git commit -m "Auto Server Commit"
-git push origin main
+git push -u https://$USERNAME:$PASSWORD@$REMOTE_REPO main >> ./logs/git_log.log
